f8ab25a56a71503bf470340e12900a70f9caf061
Mr-MichaelK <michael1kolanjian2@gmail.com>
2024-07-20 22:48:47 +0300

    Add implementation of problems 01 through 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...unwanted) {
+    const resultArr = [];
+    for (const element of arr) {
+        let wanted = true;
+        for (const temp of unwanted) {
+            if (element === temp) {
+                wanted = false;
+                break;
+            }
+        }
+        if (wanted) {
+            resultArr.push(element);
+        }
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (
+        typeof(start) != "number"  || typeof(end) != "number" ||
+        start != Math.floor(start) || end != Math.floor(end) ||
+        start < 0                  || end < 0
+    ) {
+        return "ERROR";
+    }
+    if (end > start) end++;
+    else             start++;
+    return Math.abs(start * (start - 1) / 2 - end * (end - 1) / 2);
 };
 
 // Do not edit below this line
